b55bd5859481aca4f4f6c48070ae8e13d5aca5a1
Markus Yrjölä <yrjola.markus@gmail.com>
2019-02-08 23:51:28 +0200

    The app now remembers the used date range

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import moment from 'moment';
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/es/Typography/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -21,15 +22,13 @@ const styles = theme => ({
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = Object.assign(
-      {
-        apiToken: localStorage.getItem('apiToken') || '',
-        timeEntriesByDate: {}
-      },
-      defaultDateRange
-    );
+    const { apiToken, startDate, endDate } = localStorage;
+    const dateRange =
+      startDate && endDate ? { startDate: moment(startDate), endDate: moment(endDate) } : defaultDateRange;
+    this.state = Object.assign({ apiToken, timeEntriesByDate: {} }, dateRange);
 
     this.handleDialogClose = this.handleDialogClose.bind(this);
+    this.handleDateRangeChange = this.handleDateRangeChange.bind(this);
   }
 
   componentDidMount() {
@@ -61,14 +60,17 @@ class App extends Component {
     this.setState({ apiToken });
   }
 
+  handleDateRangeChange(dateRange) {
+    Object.keys(dateRange).forEach(key => localStorage.setItem(key, dateRange[key].format('YYYY-MM-DD')));
+    this.setState(dateRange);
+  }
+
   render() {
     const { startDate, endDate, apiToken, timeEntriesByDate, error } = this.state;
     const { classes } = this.props;
 
     if (!apiToken) {
-      return (
-        <ApiTokenDialog open={!apiToken} mandatory={!apiToken} onClose={this.handleDialogClose} />
-      );
+      return <ApiTokenDialog open={!apiToken} mandatory={!apiToken} onClose={this.handleDialogClose} />;
     }
 
     const totalTimeDiff = Object.values(timeEntriesByDate).reduce((a, b) => a + b, 0);
@@ -77,11 +79,7 @@ class App extends Component {
       <React.Fragment>
         <Grid container justify="center" className={classes.dateSelectorContainer}>
           <Grid item>
-            <DateRangeSelector
-              startDate={startDate}
-              endDate={endDate}
-              onChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-            />
+            <DateRangeSelector startDate={startDate} endDate={endDate} onChange={this.handleDateRangeChange} />
           </Grid>
           {!startDate.isSame(endDate, 'month') && (
             <Grid item xs={12}>
