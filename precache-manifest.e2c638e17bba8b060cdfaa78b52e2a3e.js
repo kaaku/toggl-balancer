7a126069fa15455689c014f4cce0b6455bb48e75
Markus Yrjölä <yrjola.markus@gmail.com>
2019-05-26 14:00:37 +0300

    Updates

---
+++ precache-manifest.e2c638e17bba8b060cdfaa78b52e2a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5797fd045f70b839939a55d7ff4ccfa0",
+    "url": "/toggl-balancer/index.html"
+  },
+  {
+    "revision": "0a12334761b40a83b336",
+    "url": "/toggl-balancer/static/css/main.01488732.chunk.css"
+  },
+  {
+    "revision": "0a1576d7ec7629477659",
+    "url": "/toggl-balancer/static/js/2.fdd43bf7.chunk.js"
+  },
+  {
+    "revision": "0a12334761b40a83b336",
+    "url": "/toggl-balancer/static/js/main.f714957d.chunk.js"
+  },
+  {
+    "revision": "7fba778d9f4d79edc7c7",
+    "url": "/toggl-balancer/static/js/runtime~main.65e7b23b.js"
+  }
+]);
