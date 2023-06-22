0e1514f4de506743246dd0d8b7eeb9aa389ecd9d
Markus Yrjölä <markus.yrjola@nitor.com>
2023-06-22 11:15:18 +0300

    Migrate from JSS to Emotion for RunningEntryIndicator

--- src/RunningEntryIndicator.jsx
+++ src/RunningEntryIndicator.jsx
@@ -4,27 +4,28 @@ import Box from '@mui/material/Box';
 import Icon from '@mui/material/Icon';
 import Tooltip from '@mui/material/Tooltip';
 import { red } from '@mui/material/colors';
-import withStyles from '@mui/styles/withStyles';
 
 const spacingBySize = { small: 1, default: 1.5, large: 2 };
-const styles = (theme) => ({
-  root: {
-    display: 'inline-block',
-    padding: (props) => theme.spacing(spacingBySize[props.size] || 0),
-  },
-  icon: {
-    color: red[500],
-    verticalAlign: 'top',
-    animation: 'blinker 2s cubic-bezier(0.46, 0.03, 0.52, 0.96) infinite',
-  },
-});
 
-function RunningEntryIndicator(props) {
-  const { size, visible, classes } = props;
+export default function RunningEntryIndicator(props) {
+  const { size, visible } = props;
   return visible ? (
-    <Box className={classes.root} component="span">
+    <Box
+      sx={{
+        display: 'inline-block',
+        padding: spacingBySize[size] || 0,
+      }}
+      component="span"
+    >
       <Tooltip title="You have a running time entry" placement="top">
-        <Icon fontSize={size} className={classes.icon}>
+        <Icon
+          fontSize={size}
+          sx={{
+            color: red[500],
+            verticalAlign: 'top',
+            animation: 'blinker 2s cubic-bezier(0.46, 0.03, 0.52, 0.96) infinite',
+          }}
+        >
           fiber_manual_record
         </Icon>
       </Tooltip>
@@ -35,13 +36,9 @@ function RunningEntryIndicator(props) {
 RunningEntryIndicator.propTypes = {
   size: PropTypes.oneOf(['default', 'small', 'large']),
   visible: PropTypes.bool,
-  // eslint-disable-next-line react/forbid-prop-types
-  classes: PropTypes.object.isRequired,
 };
 
 RunningEntryIndicator.defaultProps = {
   size: 'default',
   visible: true,
 };
-
-export default withStyles(styles, { withTheme: true })(RunningEntryIndicator);
