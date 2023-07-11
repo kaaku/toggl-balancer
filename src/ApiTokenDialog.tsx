af445b38ee90046f8fa3a3a44f8a10db070c01ff
Markus Yrjölä <markus.yrjola@nitor.com>
2023-07-11 13:15:01 +0300

    Converted ApiTokenDialog into a functional component, introduced hooks

--- src/ApiTokenDialog.tsx
+++ src/ApiTokenDialog.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import Dialog from '@mui/material/Dialog';
@@ -17,77 +17,41 @@ interface Props {
   onClose: (result: { apiToken?: string; rememberMe?: boolean }) => void;
 }
 
-interface State {
-  apiToken?: string;
-  rememberMe?: boolean;
-}
+export default function ApiTokenDialog({ open, mandatory, oldApiToken, onClose = () => {} }: Props) {
+  const [apiToken, setApiToken] = useState(oldApiToken);
+  const [rememberMe, setRememberMe] = useState(true);
 
-export default class ApiTokenDialog extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      apiToken: props.oldApiToken,
-      rememberMe: true,
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleCancel = this.handleCancel.bind(this);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ apiToken: event.target.value });
-  }
-
-  handleSubmit() {
-    const { onClose } = this.props;
-    const { apiToken, rememberMe } = this.state;
-    onClose({ apiToken, rememberMe });
-  }
-
-  handleCancel() {
-    const { onClose } = this.props;
-    onClose({});
-  }
-
-  render() {
-    const { open = false, mandatory = false } = this.props;
-    const { apiToken, rememberMe } = this.state;
-
-    return (
-      <Dialog open={open} disableEscapeKeyDown={mandatory} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Enter your Toggl API Token</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Your Toggl time entries will be fetched using your API Token, which you can find from your{' '}
-            <a href="https://track.toggl.com/profile" target="_blank" rel="noopener noreferrer">
-              Toggl profile <Icon fontSize="small">open_in_new</Icon>
-            </a>
-          </DialogContentText>
-          <TextField
-            variant="standard"
-            type="password"
-            autoFocus
-            fullWidth
-            margin="dense"
-            label="API Token"
-            value={apiToken}
-            onChange={this.handleChange}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={rememberMe} onChange={(e, checked) => this.setState({ rememberMe: checked })} />
-            }
-            label="Remember me"
-          />
-        </DialogContent>
-        <DialogActions>
-          {!mandatory && <Button onClick={this.handleCancel}>Cancel</Button>}
-          <Button variant="contained" onClick={this.handleSubmit} disabled={!apiToken}>
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog open={open} disableEscapeKeyDown={mandatory} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Enter your Toggl API Token</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Your Toggl time entries will be fetched using your API Token, which you can find from your{' '}
+          <a href="https://track.toggl.com/profile" target="_blank" rel="noopener noreferrer">
+            Toggl profile <Icon fontSize="small">open_in_new</Icon>
+          </a>
+        </DialogContentText>
+        <TextField
+          variant="standard"
+          type="password"
+          autoFocus
+          fullWidth
+          margin="dense"
+          label="API Token"
+          value={apiToken}
+          onChange={(e) => setApiToken(e.target.value)}
+        />
+        <FormControlLabel
+          control={<Checkbox checked={rememberMe} onChange={(e, checked) => setRememberMe(checked)} />}
+          label="Remember me"
+        />
+      </DialogContent>
+      <DialogActions>
+        {!mandatory && <Button onClick={() => onClose({})}>Cancel</Button>}
+        <Button variant="contained" onClick={() => onClose({ apiToken, rememberMe })} disabled={!apiToken}>
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
